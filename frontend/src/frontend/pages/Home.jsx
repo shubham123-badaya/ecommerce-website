ccd9ffd8b30356b3030e8afbf7054afeda04bd9b
shubham <shubhambadaya009@gmail.com>
2025-09-16 11:11:11 +0530

    frontend route added and created file

---
+++ frontend/src/frontend/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1>This is Home Page</h1>
+      <p>Welcome to our E-commerce website 🚀</p>
+    </div>
+  );
+}
+
+export default Home;
--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,4 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// Frontend Pages
+import Home from "../frontend/pages/Home";
+
+// Admin Pages
+
 import Login from "../admin/auth/sign_in.jsx";
 // import Dashboard from "../pages/Dashboard";
 // import NotFound from "../pages/NotFound";
@@ -7,6 +12,8 @@ function AppRoutes() {
   return (
     <Router>
       <Routes>
+        {/* ---------- Frontend Routes ---------- */}
+        <Route path="/" element={<Home />} />
         {/* Superadmin Login */}
         <Route path="/admin/login" element={<Login />} />
         {/* <Route path="/admin/dashboard" element={<Dashboard />} />
