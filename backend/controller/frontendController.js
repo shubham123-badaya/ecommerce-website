7a407db138ec17566cdabca817c1084894cceed8
shubham <shubhambadaya009@gmail.com>
2025-10-02 16:38:41 +0530

    slider and search product api

--- backend/controller/frontendController.js
+++ backend/controller/frontendController.js
@@ -1,5 +1,7 @@
 import Product from "../models/Product.js";
 import Category from "../models/Category.js";
+import Slider from "../models/Slider.js";
+
 
 export const getCategoryByProduct = async (req, res) => {
   try {
@@ -28,6 +30,39 @@ export const getAllProducts = async (req, res) => {
   try {
     const products = await Product.find();
     res.status(200).json({ products });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
+
+export const searchProducts = async (req, res) => {
+  try {
+    const { q } = req.query;
+
+    if (!q) {
+      return res.status(400).json({ message: "Search query is required" });
+    }
+    const products = await Product.find({
+      title: { $regex: q, $options: "i" }
+    });
+
+    res.status(200).json({
+      query: q,
+      results: products,
+      count: products.length
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
+
+export const getSliders = async (req, res) => {
+  try {
+    const sliders = await Slider.find().sort({ createdAt: -1 });
+    res.status(200).json({
+      count: sliders.length,
+      sliders
+    });
   } catch (error) {
     res.status(500).json({ message: "Server error", error: error.message });
   }
--- backend/routes/frontendRoutes.js
+++ backend/routes/frontendRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getCategoryByProduct, getAllProducts } from "../controller/frontendController.js";
+import { getCategoryByProduct, getAllProducts,searchProducts , getSliders} from "../controller/frontendController.js";
 
 const router = express.Router();
 
 router.get("/products", getAllProducts);
 router.get("/category/products/:categoryId", getCategoryByProduct);
-
+router.get("/products/search", searchProducts);
+router.get("/sliders", getSliders);
 export default router;
